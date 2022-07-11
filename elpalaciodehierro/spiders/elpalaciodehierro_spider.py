708f4c9af87fac12ba07e0a39ad92b8a0b26f19a
nepal <nepal@nepali.com>
2022-07-11 21:53:43 +0545

    added the products

--- elpalaciodehierro/spiders/elpalaciodehierro_spider.py
+++ elpalaciodehierro/spiders/elpalaciodehierro_spider.py
@@ -1,43 +1,15 @@
 import scrapy
 import datetime
-'''
- default_dict = {
-                    'Date': datetime.now().strftime("%Y-%m-%d"),
-                    'Canal': 'Scorpion',
-                    'Category': breadcrumbs_dict.get(0, ''),
-                    'Subcategory': breadcrumbs_dict.get(1, ''),
-                    'Subcategory2': breadcrumbs_dict.get(2, ''),
-                    'Subcategory3': '',
-                    'Marca': brand,
-                    'Modelo': '',
-                    'SKU': sku,
-                    'UPC': sku,
-                    'Item': item,
-                    'Item Characteristics': item_characteristics,
-                    'URL SKU': response.url,
-                    'Image': image,
-                    'Price': '',
-                    'Sale Price': '',
-                    'Shipment Cost': '',
-                    'Sales Flag': '',
-                    'Store ID': '',
-                    'Store Name': '',
-                    'Store Address': '',
-                    'Stock': '',
-                    'UPC WM': sku.zfill(16),
-                    'Final Price': '',
-                    }
-'''
+
 
 # scrapy.spiders.SitemapSpider
 class ElpalaciodeSpider(scrapy.spiders.SitemapSpider):
     name = 'elpalaciode'
-    sitemap_urls = ['https://www.elpalaciodehierro.com/sitemap_index.xml']
+    sitemap_urls = [f'https://www.elpalaciodehierro.com/sitemap_{x}-product.xml' for x in range(0,20)]
     sitemap_rules = [(r'.html', 'parse')]
     #start_urls = ['https://www.elpalaciodehierro.com/under-armour-tenis-para-correr-shadow-hombre-41726918.html']
 
     def parse(self, response):
-
 
         try:
             item = dict()
